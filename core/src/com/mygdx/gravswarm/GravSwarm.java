f0477d65e3eed3d0606a76737dee8992d89370b6
Zealot Prime <tgslee7@gmail.com>
2016-05-02 00:57:02 -0600

    ScreenEdge warp and reflect modes, moon spawning implemented

--- core/src/com/mygdx/gravswarm/GravSwarm.java
+++ core/src/com/mygdx/gravswarm/GravSwarm.java
@@ -21,7 +21,10 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.environment.PointLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Pool;
 
 import java.util.Random;
@@ -30,35 +33,53 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class GravSwarm extends ApplicationAdapter {
-	boolean speedCheck;
-
-	Vector<Moon> moons;
-	Pool<Gravity> freeGravities;
-	Vector<Gravity>gravities;
-	Vector<GravityHandler>gravityHandlers;
-	Vector<Gravity>gravitiesToBeCulled;
-
-	PerspectiveCamera cam;
-	Model modelTemplate;
-	ModelBatch modelBatch;
-	Environment environment;
-	CameraInputController camController;
-	CoreInputProcessor coreInput;
-	CyclicBarrier barrier;
-
+	enum ClipPlane{NEAR, FAR, LEFT, RIGHT, TOP, BOTTOM};
+	enum ScreenMode{GRAVITY,PLANECHANGE,SPAWN};
+	enum EdgeMode{NONE, WARP, REFLECT};
 	float LIGHT_INTENSITIY;
 	float GRAVITY_PLANE_DISTANCE;
 	int MOONS_TO_SPAWN;
 	int THREAD_COUNT;
+	boolean speedCheck;
+	EdgeMode edgeMode;
+	ScreenMode screenMode;
+
+	Vector<Moon> moons;
+	Pool<Gravity> freeGravities;
+	Pool<Moon> freeMoons;
+	Vector<Gravity>gravities;
+	Vector<GravityHandler>gravityHandlers;
+	Vector<Gravity>gravitiesToBeCulled;
+	Vector<Moon> moonsToReposition;
+	Material moonTexture;
+	Ray warpRay;
+	Vector3 workerVec, workerVec2;
+	//Stage ui;
+
+	PerspectiveCamera cam;
+	Model modelTemplate;
+	ModelBatch modelBatch;
+	ModelInstance visualTouchPlane;
+	Environment environment;
+	//CameraInputController camController;
+	CoreInputProcessor coreInput;
+	CyclicBarrier barrier;
+
+
 	
 	@Override
 	public void create () {
 		speedCheck=false;
 		Random rnd=new Random();
+		warpRay=new Ray();
+		workerVec=new Vector3();
+		workerVec2=new Vector3();
 		moons=new Vector<Moon>();
 		gravities=new Vector<Gravity>();
 		gravitiesToBeCulled=new Vector<Gravity>();
+		moonsToReposition=new Vector<Moon>();
 		gravityHandlers=new Vector<GravityHandler>();
+		moonTexture=new Material(ColorAttribute.createDiffuse(1,1,1,1));
 		freeGravities=new Pool<Gravity>() {
 			@Override
 			protected Gravity newObject() {
@@ -66,10 +87,13 @@ public class GravSwarm extends ApplicationAdapter {
 			}
 		};
 
+
 		LIGHT_INTENSITIY=50000f;
-		GRAVITY_PLANE_DISTANCE=.7f;
-		MOONS_TO_SPAWN=1000;
-		THREAD_COUNT=3;
+		GRAVITY_PLANE_DISTANCE=.2f;
+		MOONS_TO_SPAWN=2000;
+		THREAD_COUNT=10;
+		edgeMode=EdgeMode.NONE;
+		screenMode=ScreenMode.GRAVITY;
 
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
@@ -89,25 +113,32 @@ public class GravSwarm extends ApplicationAdapter {
 
 
 		ModelBuilder modelBuilder = new ModelBuilder();
+//		modelTemplate=modelBuilder.createLineGrid(30,30,100,100,moonTexture,VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+//		visualTouchPlane=new ModelInstance(modelTemplate);
+
+		//workerVec.scl(-1000f);
+		//visualTouchPlane.transform.setToLookAt(workerVec,cam.direction);
+		//visualTouchPlane.transform.setToRotation(cam.position, 0);
+		//visualTouchPlane.transform.setToRotation(workerVec, 0);
+		//visualTouchPlane.transform.rotate(cam.up,0);
+		//visualTouchPlane.transform.setFromEulerAngles()
+
+		modelTemplate=modelBuilder.createSphere(5f, 5f, 5f, 8, 5, moonTexture, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		freeMoons=new Pool<Moon>()
+		{
+			protected Moon newObject()
+			{
+				return new Moon(modelTemplate);
+			}
+		};
 		for(int x=0;x<MOONS_TO_SPAWN;++x)
 		{
-			modelTemplate=modelBuilder.createSphere(5f, 5f, 5f, 8, 5, new Material(ColorAttribute.createDiffuse(rnd.nextFloat(),rnd.nextFloat(),rnd.nextFloat(),0)), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
 			moons.add(new Moon(modelTemplate));
 			moons.elementAt(x).transform.translate((rnd.nextFloat() * 200) - 100, (rnd.nextFloat() * 200) - 100, (rnd.nextFloat() * 200) - 100);
 		}
-		int curr=0;
-		int step=MOONS_TO_SPAWN/THREAD_COUNT;
-		int next=step;
+		assignMoons(true);
 		for(int x=0;x<THREAD_COUNT;++x)
 		{
-			if(x<THREAD_COUNT-1)
-			{
-				gravityHandlers.add(new GravityHandler(curr, next));
-				curr=next;
-				next+=step+1;
-			}
-			else
-				gravityHandlers.add(new GravityHandler(curr,-1));
 			gravityHandlers.elementAt(x).start();
 		}
 
@@ -116,8 +147,6 @@ public class GravSwarm extends ApplicationAdapter {
 
 	@Override
 	public void render() {
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		if(speedCheck)
 		{
@@ -132,16 +161,44 @@ public class GravSwarm extends ApplicationAdapter {
 			e.printStackTrace();
 		}
 		//singleCoreGravity();
+
 		for(int moonNumber=0;moonNumber<moons.size();++moonNumber)
+		{
+			if(edgeMode!=EdgeMode.NONE)
+			{
+				if(moons.elementAt(moonNumber).willBeOffscreen())
+				{
+					switch (edgeMode)
+					{
+						case WARP:
+							moons.elementAt(moonNumber).warp();
+							break;
+						case REFLECT:
+							moons.elementAt(moonNumber).reflect();
+							break;
+					}
+				}
+			}
 			moons.elementAt(moonNumber).move();
+		}
+
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		modelBatch.begin(cam);
 		modelBatch.render(moons, environment);
+		//modelBatch.render(visualTouchPlane, environment);
 		modelBatch.end();
+
 		while(gravitiesToBeCulled.size()>0)
 		{
 			freeGravities.free(gravitiesToBeCulled.elementAt(0));
 			gravities.remove(gravitiesToBeCulled.elementAt(0));
 			gravitiesToBeCulled.remove(0);
+		}
+		while(moonsToReposition.size()>0)
+		{
+			moonsToReposition.elementAt(0).transform.setToTranslation(moonsToReposition.elementAt(0).getNextPosition());
+			moonsToReposition.remove(0);
 		}
 		try {
 			barrier.await();//restart gravity calculations
@@ -149,6 +206,31 @@ public class GravSwarm extends ApplicationAdapter {
 			e.printStackTrace();
 		} catch (BrokenBarrierException e) {
 			e.printStackTrace();
+		}
+	}
+	void assignMoons(boolean firstrun)
+	{
+		int curr=0;
+		int step=(firstrun?MOONS_TO_SPAWN/THREAD_COUNT:moons.size()/THREAD_COUNT);
+		int next=step;
+		for(int x=0;x<THREAD_COUNT;++x)
+		{
+			if(x<THREAD_COUNT-1)
+			{
+				if(firstrun)
+					gravityHandlers.add(new GravityHandler(curr, next));
+				else
+					gravityHandlers.elementAt(x).setBounds(curr, next);
+				curr=next;
+				next+=step+1;
+			}
+			else
+			{
+				if(firstrun)
+					gravityHandlers.add(new GravityHandler(curr,-1));
+				else
+					gravityHandlers.elementAt(x).setBounds(curr,-1);
+			}
 		}
 	}
 
@@ -178,22 +260,30 @@ public class GravSwarm extends ApplicationAdapter {
 	{
 		Gravity[] touchGravities;
 		Vector3 worldPoint;
+		int downX, downY;
 
 		CoreInputProcessor()
 		{
 			touchGravities=new Gravity[10];
 			worldPoint=new Vector3();
+			downX=0;
+			downY=0;
 		}
 		@Override public boolean touchDown(int x, int y, int pointer, int button)
 		{
-			if(button== Input.Buttons.LEFT)
+			if(button==Input.Buttons.LEFT&&screenMode==ScreenMode.GRAVITY)
 			{
-				worldPoint=cam.unproject(new Vector3(x, y, 1f));
-				worldPoint.lerp(cam.unproject(new Vector3(x, y, 0f)),GRAVITY_PLANE_DISTANCE);
+				worldPoint.set(cam.unproject(workerVec.set(x, y, 0f)));
+				worldPoint.lerp(cam.unproject(workerVec.set(x, y, 1f)),GRAVITY_PLANE_DISTANCE);
 				touchGravities[pointer] = freeGravities.obtain();
 				touchGravities[pointer].spawn(worldPoint);
 				gravities.add(touchGravities[pointer]);
 				return true;
+			}
+			if(button==Input.Buttons.LEFT&&screenMode==ScreenMode.SPAWN)
+			{
+				downX=x;
+				downY=y;
 			}
 			return false;
 		}
@@ -201,58 +291,249 @@ public class GravSwarm extends ApplicationAdapter {
 		{
 			if(touchGravities[pointer]!=null)
 			{
-				worldPoint=cam.unproject(new Vector3(x, y, 1f));
-				worldPoint.lerp(cam.unproject(new Vector3(x, y, 0f)),GRAVITY_PLANE_DISTANCE);
+				worldPoint.set(cam.unproject(workerVec.set(x, y, 0f)));
+				worldPoint.lerp(cam.unproject(workerVec.set(x, y, 1f)),GRAVITY_PLANE_DISTANCE);
 				touchGravities[pointer].setPosition(worldPoint);
 				return true;
 			}
 			return false;
 		}
-		@Override public boolean touchUp(int x, int y, int pointer, int button)
-		{
+		@Override public boolean touchUp(int x, int y, int pointer, int button) {
 			if(button!= Input.Buttons.LEFT) return false;
-			gravitiesToBeCulled.add(touchGravities[pointer]);
-			touchGravities[pointer]=null;
-			return true;
+			if (touchGravities[pointer] != null) {
+				gravitiesToBeCulled.add(touchGravities[pointer]);
+				touchGravities[pointer] = null;
+				return true;
+			}
+			if(screenMode == ScreenMode.SPAWN)
+			{
+				for (int i = 0; i < 50; ++i) {
+					worldPoint.set(cam.unproject(workerVec.set(downX, downY, 0f)));
+					worldPoint.lerp(cam.unproject(workerVec.set(downX, downY, 1f)), GRAVITY_PLANE_DISTANCE);
+					Moon tempMoon=freeMoons.obtain();
+					tempMoon.transform.setToTranslation(worldPoint);
+					workerVec2.set(worldPoint);
+					worldPoint.set(cam.unproject(workerVec.set(x, y, 0f)));
+					worldPoint.lerp(cam.unproject(workerVec.set(x, y, 1f)), GRAVITY_PLANE_DISTANCE);
+					workerVec2.sub(worldPoint);
+					workerVec.setToRandomDirection();
+					workerVec.scl(5f);
+					workerVec.add(workerVec2);
+					workerVec.scl(-.2f);
+					tempMoon.updateVelocity(workerVec);
+					moons.add(tempMoon);
+				}
+				assignMoons(false);
+			}
+			return false;
 		}
 		@Override public boolean keyDown(int keycode)
 		{
-			if(keycode==Input.Keys.SPACE)
+			switch (keycode)
 			{
-				speedCheck=true;
-				return true;
+				case Input.Keys.SPACE:
+					speedCheck=true;
+					return true;
+				case Input.Keys.E:
+					switch (edgeMode)
+					{
+						case NONE:
+							edgeMode=EdgeMode.WARP;
+							break;
+						case WARP:
+							edgeMode=EdgeMode.REFLECT;
+							break;
+						case REFLECT:
+							edgeMode=EdgeMode.NONE;
+							break;
+					}
+					return true;
+				case Input.Keys.S:
+					screenMode=ScreenMode.SPAWN;
+					return true;
 			}
 			return false;
 		}
 		@Override public boolean keyUp(int keycode)
 		{
-			if(keycode==Input.Keys.SPACE)
+			switch (keycode)
 			{
-				speedCheck=false;
-				return true;
+				case Input.Keys.SPACE:
+					speedCheck=false;
+					return true;
+				case Input.Keys.S:
+					screenMode=ScreenMode.GRAVITY;
+					return true;
 			}
 			return false;
 		}
 	}
 
 
-	class Moon extends ModelInstance
+	class Moon extends ModelInstance implements Pool.Poolable
 	{
 		Vector3 velocity;
+		Vector3 nextPosition;
+		ClipPlane clipPlane;
 		Moon(Model template)
 		{
 			super(template);
 			velocity=new Vector3();
+			nextPosition=new Vector3();
 		}
 		void updateVelocity(Vector3 change)
 		{
 			velocity.add(change);
 		}
-		void move()
-		{
+
+		public Vector3 getNextPosition() {
+			return nextPosition;
+		}
+
+		void move() {
 			this.transform.translate(velocity);
 		}
 		void scaleVelocity(float scalar){velocity.scl(scalar);}
+		void updateNextPosition()
+		{
+			this.transform.getTranslation(nextPosition);
+			nextPosition.add(velocity);
+		}
+		public void reset()
+		{
+			velocity.set(0,0,0);
+		}
+		public boolean willBeOffscreen()
+		{
+			updateNextPosition();
+			return !cam.frustum.pointInFrustum(nextPosition);
+		}
+		private void findClipPlane()
+		{
+			float distance=Float.MAX_VALUE;
+			float testDistance;
+			updateNextPosition();
+			for(int x=0;x<6;++x)
+			{
+				testDistance=cam.frustum.planes[x].distance(nextPosition);
+				if(testDistance<distance)
+				{
+					clipPlane=ClipPlane.values()[x];
+					distance=testDistance;
+				}
+			}
+		}
+		public void warp()
+		{
+			findClipPlane();
+			moonsToReposition.add(this);
+			switch (clipPlane)
+			{
+				case NEAR:
+					workerVec.set(cam.project(this.transform.getTranslation(workerVec2)));
+					workerVec.set(nextPosition.x,nextPosition.y,1f);
+					nextPosition.set(cam.unproject(workerVec));
+					break;
+				case FAR:
+					nextPosition.set(cam.project(this.transform.getTranslation(nextPosition)));
+					workerVec.set(nextPosition.x, nextPosition.y, 0f);
+					workerVec.set(cam.unproject(workerVec));
+					workerVec2.set(nextPosition.x,nextPosition.y,1f);
+					workerVec2.set(cam.unproject(workerVec2));
+					workerVec.lerp(workerVec2,.02f);
+					nextPosition.set(workerVec);
+					break;
+				case LEFT:
+					workerVec2.set(cam.direction);
+					workerVec2.rotate(cam.up, -90);
+					warpRay.set(this.transform.getTranslation(workerVec), workerVec2);
+					Intersector.intersectRayPlane(warpRay,cam.frustum.planes[3],nextPosition);
+					break;
+				case RIGHT:
+					workerVec2.set(cam.direction);
+					workerVec2.rotate(cam.up, 90);
+					warpRay.set(this.transform.getTranslation(workerVec), workerVec2);
+					Intersector.intersectRayPlane(warpRay, cam.frustum.planes[2], nextPosition);
+					break;
+				case TOP:
+					workerVec2.set(cam.up);
+					workerVec2.rotate(cam.direction, 180);
+					warpRay.set(this.transform.getTranslation(workerVec), workerVec2);
+					Intersector.intersectRayPlane(warpRay, cam.frustum.planes[5], nextPosition);
+					break;
+				case BOTTOM:
+					warpRay.set(this.transform.getTranslation(workerVec),cam.up);
+					Intersector.intersectRayPlane(warpRay, cam.frustum.planes[4], nextPosition);
+			}
+		}
+		public void reflect()
+		{
+
+			float length;
+			//findClipPlane();
+			updateNextPosition();
+			float x=nextPosition.x;
+			float y=nextPosition.y;
+			float z=nextPosition.z;
+			workerVec.set(x, 0, 0);
+			if(!cam.frustum.pointInFrustum(workerVec))
+				x=-1;
+			else
+				x=1;
+			workerVec.set(0,y,0);
+			if(!cam.frustum.pointInFrustum(workerVec))
+				y=-1;
+			else
+				y=1;
+			workerVec.set(0,0,z);
+			if(!cam.frustum.pointInFrustum(workerVec))
+				z=-1;
+			else
+				z=1;
+
+			velocity.x*=x;
+			velocity.y*=y;
+			velocity.z*=z;
+
+
+
+//			switch (clipPlane)
+//			{
+//				case NEAR:
+//					workerVec.set(cam.direction);
+//					break;
+//				case FAR:
+//					workerVec.set(cam.direction);
+//					workerVec.scl(-1);
+//					break;
+//				case LEFT:
+//					workerVec.set(cam.direction);
+//					workerVec.rotate(cam.up, -90);
+//					break;
+//				case RIGHT:
+//					workerVec.set(cam.direction);
+//					workerVec.rotate(cam.up, 90);
+//					break;
+//				case TOP:
+//					workerVec.set(cam.up);
+//					workerVec.scl(-1);
+//					break;
+//				case BOTTOM:
+//					workerVec.set(cam.up);
+//					break;
+//			}
+//			length=velocity.len();
+//			velocity.nor();
+//			velocity.crs(workerVec);
+//			velocity.setLength(length);
+
+
+//			length=velocity.len();
+//			workerVec.set(cam.frustum.planes[clipPlane.ordinal()].normal);
+//			workerVec.scl(length);
+//			velocity.crs(workerVec);
+//			//velocity.setLength(length);
+		}
 	}
 
 	class Gravity implements Pool.Poolable
@@ -287,8 +568,7 @@ public class GravSwarm extends ApplicationAdapter {
 			this.magnitude=magnitude;
 			this.quadratic=quadratic;
 			light=new PointLight();
-			light.set(1f, 1f, 1f, position, LIGHT_INTENSITIY);
-			//environment.add(light);
+			light.set(0f, 0f, 1f, position, LIGHT_INTENSITIY);
 		}
 
 		public void setQuadratic(boolean mode)
@@ -331,6 +611,8 @@ public class GravSwarm extends ApplicationAdapter {
 			}
 			return output;
 		}
+
+
 	}
 
 	class GravityHandler extends Thread
@@ -348,7 +630,7 @@ public class GravSwarm extends ApplicationAdapter {
 			this.highBound=highBound;
 		}
 
-		public void setBounds(int high, int low)
+		public void setBounds(int low, int high)
 		{
 			lowBound=low;
 			highBound=high;
